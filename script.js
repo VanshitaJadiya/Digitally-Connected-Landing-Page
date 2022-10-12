587804e42cc85653048e9171dc68e60528745a79
VanshitaJadiya <114803806+VanshitaJadiya@users.noreply.github.com>
2022-10-12 18:53:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+gsap.from("#img1",{
+    opacity:0,
+    duration:2,
+    delay:0.5,
+    y:50
+
+})
+gsap.from("#img2",{
+    opacity:0,
+    duration:2,
+    delay:0.7,
+    x:50
+
+})
+gsap.from("#img3",{
+    opacity:0,
+    duration:2,
+    delay:0.9,
+    x:-50
+
+})
+gsap.from("#main h1",{
+    opacity:0,
+    duration:2,
+    delay:0.2,
+    x:50
+
+})
